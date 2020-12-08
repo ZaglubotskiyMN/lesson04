6553432cc9fce71bdfa662be495d81704702447f
ZaglubotskiyMN <zaglubotskii@gmail.com>
2020-12-08 16:04:36 +0400

    Initial commit

---
+++ src/part1/lesson04/task01/Main.java
@@ -0,0 +1,19 @@
+package part1.lesson04.task01;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 int a = 6;
+	 int b = 5;
+
+	 if (a<b)
+	     System.out.println(a);
+	 else
+	     System.out.println(b);
+
+    }
+}
+
+
+
+
---
+++ src/part1/lesson04/task01/Zadanie_2.java
@@ -0,0 +1,22 @@
+package part1.lesson04.task01;
+
+public class Zadanie_2 {
+
+    public static void main(String[] args) {
+
+
+        int x = 0;// Введите значение
+
+        if (x>0)
+            System.out.println("Положительное");
+        if (x<0)
+            System.out.println("Отрицательное");
+        if (x==0)
+            System.out.println("Нулевое");
+
+
+
+
+    }
+}
+
