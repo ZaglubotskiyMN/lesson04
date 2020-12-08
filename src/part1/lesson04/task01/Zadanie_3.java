797bd37dd456804037885ba41e03278ea2d31c14
ZaglubotskiyMN <zaglubotskii@gmail.com>
2020-12-08 19:42:33 +0400

    +Zadanie 3

---
+++ src/part1/lesson04/task01/Zadanie_3.java
@@ -0,0 +1,17 @@
+package part1.lesson04.task01;
+
+public class Zadanie_3 {
+    public static void main(String[] args) {
+        for (int x=1;x<=9;x++){
+            for (int y=1;y<=9;y++){
+                System.out.print(y*x+" ");
+            }
+            System.out.println("");
+        }
+
+
+
+
+
+    }
+}
