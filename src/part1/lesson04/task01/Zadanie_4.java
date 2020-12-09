f47e47bc44430c5595bd06930cdd3cdb39d258cd
ZaglubotskiyMN <zaglubotskii@gmail.com>
2020-12-09 14:09:06 +0400

    Zadanie 4

--- src/part1/lesson04/task01/Zadanie_4.java
+++ src/part1/lesson04/task01/Zadanie_4.java
@@ -2,6 +2,18 @@ package part1.lesson04.task01;
 
 public class Zadanie_4 {
     public static void main(String[] args) {
+    int n = 4; //Число членов последовательности
+    int a1 = 1;// первый член прогресси
+    int d = 2; //ввести разность
+    int an;
+    int sum;
+
+    an=a1+(n-1)*d;
+
+    System.out.println(an+ "-" +"N-ый член арифметической прогресси");
+
+    sum=(a1+an)*n/2;
+    System.out.print(sum+"-"+"Сумма первых n членов арифметической прогрессии");
 
 
 
